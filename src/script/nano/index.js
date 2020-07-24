dbe86b0c344a7e3eccfd21ec916bfaf3fade29f7
imsourceboom <ecmascript.developer@gmail.com>
2020-07-24 16:48:47 +0900

    remove console.log

--- src/script/nano/index.js
+++ src/script/nano/index.js
@@ -2,16 +2,12 @@ const nanos = Array.from(document.querySelectorAll(".amount input"));
 
 if (nanos !== null) {
   nanos.map((nano) => {
-    console.log(nano);
     let value = nano.value;
     // text = text.replace(/,|\./gi, "");
     value = parseFloat(value);
     value = value / 1000000000;
-    console.log(value);
     value = value.toFixed(4);
     value = value.toString();
-
-    console.log(value);
 
     let divide = value.split(".");
     divide[0] = parseFloat(divide[0]).toLocaleString();
