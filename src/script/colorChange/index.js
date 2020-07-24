5ec56ef20a200172a6d488500736db362a724bed
imsourceboom <ecmascript.developer@gmail.com>
2020-07-24 14:26:47 +0900

    body background color bug fix test

--- src/script/colorChange/index.js
+++ src/script/colorChange/index.js
@@ -1,17 +1,22 @@
 const colorChangeButton = document.querySelector("body > header .color-change");
+const body = document.querySelector("body");
 
 if (colorChangeButton !== null) {
   colorChangeButton.addEventListener("click", () => {
     if (colorChangeButton.classList.contains("brightness")) {
       colorChangeButton.classList.remove("brightness");
       colorChangeButton.classList.add("darkness");
-      document.body.style.backgroundColor = "rgba(47, 53, 66, 1.0)";
-      document.body.style.color = "rgba(255, 255, 255, 1.0)";
+      body.classList.remove("brightness");
+      body.classList.add("darkness");
+      //   document.body.style.backgroundColor = "rgba(47, 53, 66, 1.0)";
+      //   document.body.style.color = "rgba(255, 255, 255, 1.0)";
     } else if (colorChangeButton.classList.contains("darkness")) {
       colorChangeButton.classList.remove("darkness");
       colorChangeButton.classList.add("brightness");
-      document.body.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
-      document.body.style.color = "rgba(29, 31, 31, 1.0)";
+      body.classList.remove("darkness");
+      body.classList.add("brightness");
+      //   document.body.style.backgroundColor = "rgba(255, 255, 255, 1.0)";
+      //   document.body.style.color = "rgba(29, 31, 31, 1.0)";
     }
   });
 }
