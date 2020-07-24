1b41a7458e171d11e396a538c77eeb862e31ba97
imsourceboom <ecmascript.developer@gmail.com>
2020-07-24 15:44:39 +0900

    basic layout complete

--- src/script/preventDefault/index.js
+++ src/script/preventDefault/index.js
@@ -1,9 +1,16 @@
 const anchors = Array.from(document.querySelectorAll("main a"));
+const footerAnchor = document.querySelector("footer a");
 
 if (anchors != null) {
   anchors.map((anchor) => {
     anchor.addEventListener("click", (e) => {
       e.preventDefault();
     });
+  });
+}
+
+if (footerAnchor !== null) {
+  footerAnchor.addEventListener("click", (e) => {
+    e.preventDefault();
   });
 }
