f826a463f0d882edf577fa9f648e5b4868d7996e
imsourceboom <ecmascript.developer@gmail.com>
2020-07-25 19:51:56 +0900

    remove value filter

--- src/script/nano/index.js
+++ src/script/nano/index.js
@@ -4,37 +4,37 @@ const nanos = Array.from(document.querySelectorAll(".amount input"));
 
 if (nanos !== null) {
   nanos.map((nano) => {
-    if (nano.value !== "") {
-      let value = nano.value;
-      // text = text.replace(/,|\./gi, "");
-      value = parseFloat(value);
-      value = value / 1000000000;
-      value = value.toFixed(4);
-      let divide = value.split(".");
-      const integerOptions = {
-        // decimalPlaces: 4,
-        duration: 2.5,
-      };
-      const decimalOptions = {
-        prefix: ".",
-        // suffix: " GRAM",
-        useGrouping: false,
-        duration: 1.5,
-        // separator: "",
-      };
-      const integerAni = new CountUp(
-        nano.nextElementSibling,
-        divide[0],
-        integerOptions
-      );
-      const decimalAni = new CountUp(
-        nano.nextElementSibling.nextElementSibling,
-        divide[1],
-        decimalOptions
-      );
-      integerAni.start();
-      decimalAni.start();
-    }
+    // if (nano.value !== "") {
+    let value = nano.value;
+    // text = text.replace(/,|\./gi, "");
+    value = parseFloat(value);
+    value = value / 1000000000;
+    value = value.toFixed(4);
+    let divide = value.split(".");
+    const integerOptions = {
+      // decimalPlaces: 4,
+      duration: 2.5,
+    };
+    const decimalOptions = {
+      prefix: ".",
+      // suffix: " GRAM",
+      useGrouping: false,
+      duration: 1.5,
+      // separator: "",
+    };
+    const integerAni = new CountUp(
+      nano.nextElementSibling,
+      divide[0],
+      integerOptions
+    );
+    const decimalAni = new CountUp(
+      nano.nextElementSibling.nextElementSibling,
+      divide[1],
+      decimalOptions
+    );
+    integerAni.start();
+    decimalAni.start();
+    // }
 
     // divide[0] = parseFloat(divide[0]).toLocaleString();
     // const value = `${divide[0]}.${divide[1]}`;
